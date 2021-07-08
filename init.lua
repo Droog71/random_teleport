7d370fdcd03b402647023f430efcf2b4b488155f
Droog71 <71droog71@gmail.com>
2021-07-07 21:34:51 -0400

    Added version info. Fixed indentation.

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 --[[
     Minetest Random Teleport Mod
-    GNU Affero General Public License version 3 (AGPLv3)
+    Version: 1.0.0
+    License: GNU Affero General Public License version 3 (AGPLv3)
 ]]--
 
 -- Register the random teleport command "/rtp".
@@ -10,23 +11,23 @@ minetest.register_chatcommand("rtp", {
         interact = true,
     },
     func = function(name)
-		local player = minetest.get_player_by_name(name) -- The player who entered the command.
+	    local player = minetest.get_player_by_name(name) -- The player who entered the command.
         local old_pos = player:getpos() -- The current position of the player.
             
         -- Create a random position.
-		local x = math.random(-30000, 30000)
-		local y = 0
-		local z = math.random(-30000, 30000)
-		local pos = vector.new(x, y, z)
+        local x = math.random(-30000, 30000)
+        local y = 0
+        local z = math.random(-30000, 30000)
+        local pos = vector.new(x, y, z)
         local failed = false
 
         -- Move the player to the random position.
-		player:setpos(pos)
+        player:setpos(pos)
 
         -- Wait 3 seconds for the world to generate around the player, then move the player to the surface.
         minetest.after(3, function()
             local node_name = minetest.get_node(player:getpos()).name -- Get the name of the node at the player's position.
-                    
+        
             while node_name ~= "air" do
 
                 -- Move the player up 1 node on the y axis and update the node_name variable.
@@ -40,13 +41,13 @@ minetest.register_chatcommand("rtp", {
                     player:setpos(old_pos)
                     failed = true
                 end
-                        
+        
             end
-                    
+        
             if failed == true then
                 minetest.chat_send_player(name, "Teleportation failed! Returned to " .. minetest.pos_to_string(player:getpos()) .. ".")
             else
-		        minetest.chat_send_player(name, "Teleported to " .. minetest.pos_to_string(player:getpos()) .. ".")
+                minetest.chat_send_player(name, "Teleported to " .. minetest.pos_to_string(player:getpos()) .. ".")
             end
         end)
     end,
